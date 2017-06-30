e30e4d3da51196bc4597b895faf7bb8ca8945c09
jeremieRoth <rothjeremie@yahoo.fr>
2017-06-30 16:01:17 +0200

    add testjson file and read json code

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask;
 from flask import render_template;
-
+import json;
+import logging
+from logging.handlers import RotatingFileHandler
 app = Flask(__name__, template_folder='./view')
 
 @app.route('/')
@@ -14,6 +16,36 @@ def kappa():
 @app.route('/issou')
 def issou():
     return 'issou'
+
+
+@app.route('/json')
+def jsonParse():
+	
+
+	json_data=open("json").read()
+	data=json.loads(json_data)
+	
+	chaine="<html><head></head><body>"
+	# if(is_array(data))
+	for key in data:
+		chaine+="<p>"+str(key)+" : </p>"
+		if isinstance(data[key], str):
+			chaine+="<p>------"+data[key]+"</p>"
+		# else:
+		# 	for key2 in data[key]:
+		# 		chaine+="<p>------"+str(key2)+" : </p>"
+		# 		if isinstance(data[key][key2], list):
+		# 			chaine+="<p>------------is dict</p>"
+		# 		elif isinstance(data[key][key2], str):
+		# 			chaine+="<p>------------"+data[key][key2]+"</p>"
+
+
+
+	chaine+="</body></html>"
+
+	return chaine;
+	
+
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port='80',debug=True)
