d51126ea67902cc2de99b1748388b7d1f4c564f6
gilian dupuis <gilian.dupuis@orange.fr>
2017-06-30 10:42:39 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask;
+from flask import render_template;
+
+app = Flask(__name__, template_folder='./view')
+
+@app.route('/')
+def index():
+    return render_template('index.html');
+
+@app.route('/kappa')
+def kappa():
+    return ':kappa:'
+
+@app.route('/issou')
+def issou():
+    return 'issou'
+
+__name__ =='__main__';
+		app.run(host=0.0.0.0, port=80)
