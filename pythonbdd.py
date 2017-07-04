2f7132817df31b40365fb4d5f5da5a871ce6c7ee
gilian dupuis <gilian.dupuis@orange.fr>
2017-07-04 13:41:58 +0200

    request mongo

--- pythonbdd.py
+++ pythonbdd.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from array import array
 client = MongoClient()
 
 client = MongoClient('localhost', 27017)
@@ -6,12 +7,22 @@ client = MongoClient('localhost', 27017)
 #db = client.movie
 
 db = client.projet
-def  db():
+def  getAll():
     base = client.projet
     #collection = client['pr']
-    collection = base.movie.find()
+    collection = base.inventory.find()
     return collection
 
+def getClass(name):
+    base = client.projet
+    collection = [base.inventory.find_one({"name" : name})]
+    return collection
+
+def getSClass(id):
+    base = client.projet
+    collection =  [base.inventory.find({ "sClasse : { }" })]
+    return collection
+        
 # import datetime
 
 # post = { "author": "mike",
--- server.py
+++ server.py
@@ -1,20 +1,26 @@
-from flask import Flask;
-from flask import render_template;
-import pythonbdd;
-app= Flask(__name__, template_folder='./view', static_url_path='/view/assets')
+from flask import Flask
+from flask import render_template
+import pythonbdd
+
+app = Flask(__name__, template_folder='./view', static_url_path='/view/assets')
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/kappa')
-def kappa():
-	return ':kappa'
+@app.route('/classe/<name>')
+def classes(name):
+    data = pythonbdd.getClass(name)
+    return render_template('mongo.html',data=data) 
 
-@app.route('/issou')
-def issou():
-    data = pythonbdd.db()
-    
+@app.route('/sClasses')
+def sClasses():
+    #data = pythonbdd.getSClass(id)
+    return render_template('mongo.html')
+
+@app.route('/all')
+def all():
+    data = pythonbdd.getAll()
     return render_template('mongo.html',data=data) #render_template('mongo.html', data=data)
 
 if __name__ == '__main__':
