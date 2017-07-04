aaa2eb2450d186d978d7697e2ec597d019610d5b
jeremieRoth <rothjeremie@yahoo.fr>
2017-07-04 13:29:23 +0200

    advance yolo power

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask;
+from flask import Flask , request;
 from flask import render_template;
+from flask import redirect, url_for;
+import xml.etree.ElementTree as ET
 import json;
-import logging
+import logging;
 from logging.handlers import RotatingFileHandler
 app = Flask(__name__, template_folder='./view')
 
@@ -18,32 +20,66 @@ def issou():
     return 'issou'
 
 
+
+
+
+@app.route('/recuptext', methods=['GET', 'POST'])
+def recupTxt():
+	if request.method == 'POST':
+		# return "{file}".format(file=request.form['file'])
+		return redirect(url_for('text',url=request.form['txt']))
+	return render_template('recup.html');
+
+@app.route('/text/<url>')
+def text(url):
+	return url
+
+
+
+@app.route('/recupFile', methods=['GET', 'POST'])
+def recupFile():
+	if request.method == 'POST':
+		file = request.files['file']
+
+		global json_file 
+		json_file= '/tmp/'+file.filename
+
+		file.save(json_file)
+		if file.filename.endswith((".json")):
+			return redirect(url_for('jsonParse'))
+		# return "{file}".format(file=request.form['file'])
+		# return redirect(url_for('jsonParse',file2=request.file['file']))
+	return render_template('recupFile.html');
+
+
 @app.route('/json')
 def jsonParse():
-	
 
-	json_data=open("json").read()
-	data=json.loads(json_data)
-	
-	chaine="<html><head></head><body>"
+	return json_file
+
+	# json_data=open(file2).read()
+	# data=json.loads(json_data)
+	# return data["menu"]
+	# chaine="<html><head></head><body>"
+
 	# if(is_array(data))
-	for key in data:
-		chaine+="<p>"+str(key)+" : </p>"
-		if isinstance(data[key], str):
-			chaine+="<p>------"+data[key]+"</p>"
-		# else:
-		# 	for key2 in data[key]:
-		# 		chaine+="<p>------"+str(key2)+" : </p>"
-		# 		if isinstance(data[key][key2], list):
-		# 			chaine+="<p>------------is dict</p>"
-		# 		elif isinstance(data[key][key2], str):
-		# 			chaine+="<p>------------"+data[key][key2]+"</p>"
+	# for key in data:
+	# 	chaine+="<p>"+str(key)+" : </p>"
+	# 	if isinstance(data[key], str):
+	# 		chaine+="<p>------"+data[key]+"</p>"
+	# 	else:
+	# 		for key2 in data[key]:
+	# 			chaine+="<p>------"+str(key2)+" : </p>"
+	# 			if isinstance(data[key][key2], list):
+	# 				chaine+="<p>------------is dict</p>"
+	# 			elif isinstance(data[key][key2], str):
+	# 				chaine+="<p>------------"+data[key][key2]+"</p>"
 
 
 
-	chaine+="</body></html>"
+	# chaine+="</body></html>"
 
-	return chaine;
+	# return chaine;
 	
 
 
