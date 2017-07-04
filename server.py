923d0c2de847459d4ca01cf699304d4aa5e25fcb
gilian dupuis <gilian.dupuis@orange.fr>
2017-07-04 11:30:24 +0200

    bootstrap

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask;
 from flask import render_template;
 import pythonbdd;
-app= Flask(__name__, template_folder='./view')
+app= Flask(__name__, template_folder='./view', static_url_path='/view/assets')
 
 @app.route('/')
 def index():
@@ -14,6 +14,7 @@ def kappa():
 @app.route('/issou')
 def issou():
     data = pythonbdd.db()
+    
     return render_template('mongo.html', data=data)
 
 if __name__ == '__main__':
